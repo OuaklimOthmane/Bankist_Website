6cc73a240867bdda07c355522487084cc8b9bf4b
Ouaklim Othmane <ouaklimothmane@gmail.com>
2021-12-20 17:58:23 +0100

    Implementing page navigation using Event Delegation

--- script.js
+++ script.js
@@ -53,10 +53,26 @@ btnScrollTo.addEventListener("click", function (e) {
 });
 
 //! Page navigation :
+/* 
 document.querySelectorAll(".nav__link").forEach((element) => {
   element.addEventListener("click", function (e) {
     e.preventDefault(); // prevent scrolling
     const id = this.getAttribute("href"); //? we use "this.getAttribute()" instead of the "this.href" because in this case we need just the relative path.
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   });
+});
+ */
+// TODO : It will be fine of couple of elements,but what if we had much more like 1000 elements ? if we were to attch an event handler to this number like we did before with the forEach function,then we would effectively be creating 1000 copies of the same callback function like we did before and so that would then certainly impact the performance and it's really just not a clean solution in that case, and the best one without doubt is to use events delegation, so in this case we use the fact that events bubble up , and we do that by putting the eventListener on a commom parent of all the elements that we are interested in ,therfore in this case is the container "nav__links" that's around all of these links,then when the user clicks one of the links the events is generated and bubbles up and then we can basically catch that event in this common parent element and handle it there,because we also know where the event actually originated
+
+//! Page navigation using Event Delegation :
+//* Add event listener to common parent element :
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //* Matching strategy :
+  if (e.target.classList.contains(".nav__link")) {
+    // Determine what element originated the event(the target) and set a condition 'cause we actually only want to work with the clicks that happend on one of the links
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
